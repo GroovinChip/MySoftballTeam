2869ffa8334ec40a287d44619afedf99bc234f6a
Reuben Turner <reuben.turner42@gmail.com>
2018-06-28 20:58:47 -0400

    lay early groundwork for editing stats

---
+++ lib/widgets/editStatsModal.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:async/async.dart';
+import 'package:my_softball_team/globals.dart' as globals;
+
+class EditStatsModal extends StatefulWidget {
+  @override
+  _EditStatsModalState createState() => _EditStatsModalState();
+}
+
+class _EditStatsModalState extends State<EditStatsModal> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
